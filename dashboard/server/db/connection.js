09ccde47e6f9998187fa1bd8e9325197619451e5
Jayden <jaytan3966@gmail.com>
2024-08-22 18:27:54 -0700

    server fixes

--- dashboard/server/db/connection.js
+++ dashboard/server/db/connection.js
@@ -9,13 +9,17 @@ const client = new MongoClient(uri, {
     }
 })
 
-try {
-    await client.connect();
-    await client.db("ninjabalances").command({ping:1});
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (err) {
-    console.error(err);
+async function connectToDataBase(){
+    try {
+        await client.connect();
+        await client.db("ninjabalances").command({ping:1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (err) {
+        console.error(err);
+    }
 }
+
+connectToDataBase();
 
 let db = client.db("ninjabalances");
 
