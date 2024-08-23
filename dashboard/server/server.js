da206425cdb649e3677afed2932582d6da4c7cce
Jayden <jaytan3966@gmail.com>
2024-08-23 13:22:19 -0700

    changed cors origin to backend url

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -5,7 +5,7 @@ import ninjas from "./routes/records.js";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors({ origin: 'https://ninjabucksdashboard.onrender.com' }));
+app.use(cors({ origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }));
 app.use(express.json());
 app.use("/records", ninjas);
 
