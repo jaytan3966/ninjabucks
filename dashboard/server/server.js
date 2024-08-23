7dfd825dd1d66ddde71849dfc8d6275a863179b3
Jayden <jaytan3966@gmail.com>
2024-08-23 15:51:10 -0700

    test

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -5,7 +5,8 @@ import ninjas from "./routes/records.js";
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors({ origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }));
+// { origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }
+app.use(cors());
 app.use(express.json());
 app.use("/records", ninjas);
 
