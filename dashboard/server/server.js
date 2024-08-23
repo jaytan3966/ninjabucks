b4fe3c35e7cd36354933a8c6dbae1ef0654fe1b4
Jayden <jaytan3966@gmail.com>
2024-08-23 15:56:27 -0700

    changed server listening

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -5,11 +5,10 @@ import ninjas from "./routes/records.js";
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// { origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }
-app.use(cors());
+app.use(cors({ origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }));
 app.use(express.json());
 app.use("/records", ninjas);
 
-app.listen(PORT, () =>{
+app.listen(PORT, '0.0.0.0', () =>{
     console.log(`Server is running on port ${PORT}`)
 })
