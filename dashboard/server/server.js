72b9f7a6195c92f21c207717a1cba5845b2555c9
Jayden <jaytan3966@gmail.com>
2024-08-22 17:25:53 -0700

    Updated cors origin

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -5,7 +5,7 @@ import ninjas from "./routes/records.js";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://ninjabucksdashboard.onrender.com' }));
 app.use(express.json());
 app.use("/records", ninjas);
 
