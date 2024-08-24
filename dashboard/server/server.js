bfa54c9a82011ddf8de3099b9ccaeb4f55e81804
Jayden <jaytan3966@gmail.com>
2024-08-24 15:11:43 -0700

    Changed port number

--- dashboard/server/server.js
+++ dashboard/server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import ninjas from "./routes/records.js";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors({ origin: 'https://ninjabucksdashboard-fjcn.onrender.com' }));
