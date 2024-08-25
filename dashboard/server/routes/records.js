4c33d7a3988619cb89183439a27b838b212cbc0f
Jayden <jaytan3966@gmail.com>
2024-08-25 15:59:15 -0700

    added server error handling

--- dashboard/server/routes/records.js
+++ dashboard/server/routes/records.js
@@ -163,7 +163,7 @@ router.post("/login", async (req, res) => {
             return res.status(401).json({ message: "Invalid credentials" });
         }
     } catch (err) {
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ message: err });
     }
 });
 
